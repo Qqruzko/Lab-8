60b66931edbd560f03e91963fbcda3880b45d416
Qqruzko <150928737+Qqruzko@users.noreply.github.com>
2023-11-27 23:15:00 +0300

    Add files via upload

---
+++ Lab6/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            init();
+            Console.ReadLine();
+        }
+        static void init()
+        {
+            Triangle triangle1;
+            Console.WriteLine("Enter first side of trinagle");
+            int AB = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second side of trinagle");
+            int BC = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter third side of trinagle");
+            int CA = int.Parse(Console.ReadLine());
+            triangle1 = new Triangle(AB,BC,CA);
+            
+            Console.WriteLine("You entered: side a={0},side b={1},side c={2}", triangle1.pAB(),triangle1.pBC(), triangle1.pCA());
+            
+            bool real = triangle1.real_triangle(triangle1);
+            Console.WriteLine("Triangle exists ={0}",real);
+            if (real == true)
+            {
+              int tperimeter=  triangle1.triangle_perimeter(triangle1);
+                Console.WriteLine("Perimeter ={0}", tperimeter);
+                double tS = triangle1.triangle_space(triangle1, tperimeter);
+                Console.WriteLine("S of your triangle = {0}", tS);
+            }
+
+
+        }
+        
+
+         
+        }
+    }
+
---
+++ Lab6/Triangle.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class Triangle
+    {
+    public Triangle(int AB,int BC,int CA)
+    {
+        
+        tAB = AB;
+        tBC = BC;
+        tCA = CA;
+    }
+    private  int tAB = 0;
+    private  int tBC = 0;
+    private  int tCA = 0;
+    public int pAB()
+    {
+        return (tAB);
+    }
+    public int pBC()
+    {
+        return (tBC);
+    }
+    public int pCA()
+    {
+        return (tCA);
+    }
+    public bool real_triangle(Triangle tri)
+    {
+        if (tri.pAB() + tri.pBC() < tri.pCA() || tri.pBC() + tri.pCA() < tri.pAB() || tri.pAB() + tri.pBC() < tri.pCA())
+        {
+            return (false);
+            
+        }
+        else
+            if (tri.pAB() + tri.pBC() > tri.pCA() || tri.pBC() + tri.pCA() > tri.pAB() || tri.pAB() + tri.pBC() > tri.pCA())
+        {
+            return (true);
+            
+        }
+            return (false);
+
+    }
+    public int triangle_perimeter(Triangle tri)
+    {
+        int perimeter = tri.pBC() + tri.pCA() + tri.pCA();
+        
+        
+        return (perimeter);
+    }
+    public double triangle_space(Triangle tri, int perimeter)
+    {
+        int p = perimeter / 2;
+        double S = Math.Sqrt(p * (p - tri.pAB()) * (p - tri.pBC()) * (p - tri.pCA()));
+        return (S);
+    }
+}
+
